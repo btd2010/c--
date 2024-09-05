e474804a282e6a113d77db141b09f954fc59f0af
Ben Daws <ben@bendaws.net>
2024-09-05 13:29:32 -0500

    more stuff

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-![C-- logo](https://github.com/btd2010/cmm/blob/main/lib/cmm-150.png?raw=true)
+![C-- logo](lib/icons/cmm-150.png)
 # C-- ![Issues](https://img.shields.io/github/issues/btd2010/bs) ![PRs](https://img.shields.io/github/issues-pr/btd2010/bs) ![Version](https://img.shields.io/badge/version-0.0.1--beta-blue)
 C-- is a compiled, simple programming language mostly like C#, C++, and [Carbon](https://github.com/carbon-language/carbon-lang). I work on it in my free time so I can learn the programming languages I need to use faster and better.
 
@@ -30,8 +30,6 @@ fn main(string args) int {
 
   var printStatement string = "Hello world";
   std.print(printStatement);
-
-  extern_c("alloc(4096)");
 
   return 0;
 }
---
+++ compiler/main.h
@@ -0,0 +1,18 @@
+/********************************************************************
+*   C-- compiler
+*   Copyright (C) 2024 Ben Daws.
+*
+*   This program is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*
+*   If you did not receive a copy of the GNU General Public License v3,
+*   see <https://www.gnu.org/licenses>.
+********************************************************************/
+
+/***************************
+*   Name: main.c
+*   Authors: Ben Daws <ben@bendaws.net>
+*   Description: Main compiler binary.
+***************************/
